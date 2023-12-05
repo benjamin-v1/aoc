615a04a5eea45e64330d7e0ccf330679d16529c1
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-12-05 13:25:15 +0000

    init day5

---
+++ aoc/2023/day5.py
@@ -0,0 +1,36 @@
+from aocd import get_data
+data = get_data(year=2023, day=5)
+
+### PART A
+
+example_data_a = """
+"""
+
+example_answer_a = 0
+
+def part_a(data):
+    answer = 0
+
+    return answer
+
+print(part_a(example_data_a))
+
+assert part_a(example_data_a) == example_answer_a, "Example for part a is wrong!"
+print("Part a is: " + str(part_a(data)))
+
+
+### PART B
+
+# example_data_b = """
+# """
+
+# example_answer_b = 0
+
+
+# def part_b(data):
+#     answer = 0
+
+#     return answer
+
+# assert part_b(example_data_b) == example_answer_b, "Example for part b is wrong!"
+# print("Part b is: " + str(part_b(data)))
